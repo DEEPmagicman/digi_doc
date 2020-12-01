a6ff808deaecc886a21d327c039ab0bd720076ff
Raj G <rajgandhi784@gmail.com>
2020-12-01 12:12:09 +0530

    last commit

--- app/src/main/java/com/example/digitaldoctor/Register.java
+++ app/src/main/java/com/example/digitaldoctor/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,11 +16,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+
+    private static final String TAG = "Register";
+
+     static final String KEY_NAME = "full_name";
+     static final String KEY_EMAIL = "email";
+     static final String KEY_PH_NO = "ph_no";
+     static final String KEY_DOB = "dob";
 
     EditText mName, mEmail, mPassword, mPhone, mDob;
     Button mPatient, mDoctor, mPharmacy;
@@ -28,11 +42,13 @@ public class Register extends AppCompatActivity {
     String Gender;
     RadioButton male, female, other;
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         mName = findViewById(R.id.fullName);
         mEmail = findViewById(R.id.email);
         mPassword = findViewById(R.id.password);
@@ -48,6 +64,7 @@ public class Register extends AppCompatActivity {
         mDob = findViewById(R.id.dob);
 
 
+
         if(mAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
             finish();
@@ -59,6 +76,33 @@ public class Register extends AppCompatActivity {
             public void onClick(final View view) {
                 String email = mEmail.getText().toString().trim();
                 String pass = mPassword.getText().toString().trim();
+
+                String full_name = mName.getText().toString();
+                String ph_no = mPhone.getText().toString();
+                String dob = mDob.getText().toString();
+
+                Map<String, Object> reg = new HashMap<>();
+                reg.put(KEY_NAME, full_name);
+                reg.put(KEY_EMAIL, email);
+                reg.put(KEY_PH_NO, ph_no);
+                reg.put(KEY_DOB, dob);
+
+                db.collection("User").document("Patient").set(reg)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Register.this, "Data Saved", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Register.this, "Data not Saved", Toast.LENGTH_SHORT).show();
+                                Log.d(TAG, e.toString());
+                            }
+                        });
+
+
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is required");
@@ -96,6 +140,33 @@ public class Register extends AppCompatActivity {
                 String email = mEmail.getText().toString().trim();
                 String pass = mPassword.getText().toString().trim();
 
+
+                String full_name = mName.getText().toString();
+                String ph_no = mPhone.getText().toString();
+                String dob = mDob.getText().toString();
+
+                Map<String, Object> reg = new HashMap<>();
+                reg.put(KEY_NAME, full_name);
+                reg.put(KEY_EMAIL, email);
+                reg.put(KEY_PH_NO, ph_no);
+                reg.put(KEY_DOB, dob);
+
+                db.collection("User").document("Doctor").set(reg)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Register.this, "Data Saved", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Register.this, "Data not Saved", Toast.LENGTH_SHORT).show();
+                                Log.d(TAG, e.toString());
+                            }
+                        });
+
+
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is required");
                     return;
@@ -131,6 +202,32 @@ public class Register extends AppCompatActivity {
             public void onClick(final View view) {
                 String email = mEmail.getText().toString().trim();
                 String pass = mPassword.getText().toString().trim();
+
+                String full_name = mName.getText().toString();
+                String ph_no = mPhone.getText().toString();
+                String dob = mDob.getText().toString();
+
+                Map<String, Object> reg = new HashMap<>();
+                reg.put(KEY_NAME, full_name);
+                reg.put(KEY_EMAIL, email);
+                reg.put(KEY_PH_NO, ph_no);
+                reg.put(KEY_DOB, dob);
+
+                db.collection("User").document("Pharmacy").set(reg)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Register.this, "Data Saved", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Register.this, "Data not Saved", Toast.LENGTH_SHORT).show();
+                                Log.d(TAG, e.toString());
+                            }
+                        });
+
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is required");
